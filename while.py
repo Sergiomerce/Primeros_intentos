7693cd527992ee92526f938a73d0edf5e7c802bb
Sergiomerce <sergiomercesanz@gmail.com>
2020-01-12 22:10:42 +0100

    Estos son mis primeros programas

--- while.py
+++ while.py
@@ -1,6 +1,6 @@
-num_inicial=10
+num_inicial= int(input("Escribe un numero"))
 print(num_inicial)
 while num_inicial>0:
-    num_inicial-=1
+    num_inicial -= 1
     print(num_inicial)
 print("Has terminado")
